0dda91f920f868fec944bd41ffe3fbb95235a5ae
Nisa Jamalia Nurhayati <nisajamalia99@gmail.com>
2025-12-02 22:57:25 +0700

    halo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,4 +8,11 @@
 export default {
   name: "App",
 };
+
+const api = process.env.REACT_APP_API_URL;
+
+fetch(`${api}/api/products`)
+  .then(res => res.json())
+  .then(data => console.log(data));
+
 </script>
