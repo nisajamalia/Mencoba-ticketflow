ab647f86ec4e08d5d6b4cbde9390a32676bd4ae6
Nisa Jamalia Nurhayati <nisajamalia99@gmail.com>
2025-12-04 01:40:29 +0700

    mau turu

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,11 +8,4 @@
 export default {
   name: "App",
 };
-
-const api = process.env.REACT_APP_API_URL;
-
-fetch(`${api}/api/products`)
-  .then(res => res.json())
-  .then(data => console.log(data));
-
 </script>
