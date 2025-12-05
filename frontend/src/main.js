223bdb4121fae2ebf97b9b685c286c4d3186348b
Nisa Jamalia Nurhayati <nisajamalia99@gmail.com>
2025-12-05 16:12:24 +0700

    -

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-
 import App from "./App.vue";
 import router from "./router";
 import "./style.css";
