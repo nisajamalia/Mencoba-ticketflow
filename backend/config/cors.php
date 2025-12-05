0d861bc49e3021dd1dec3ce4ae958255ff3c00ff
Nisa Jamalia Nurhayati <nisajamalia99@gmail.com>
2025-12-05 16:33:55 +0700

    Update CORS to allow Vercel frontend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,9 +19,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://mencoba-ticketflow.vercel.app',
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:3000',
+    ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/.*\.vercel\.app$/',
+    ],
 
     'allowed_headers' => ['*'],
 
